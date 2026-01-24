db3a652458d62f6e0ee066624fd7434e8cbf4f23
Ondoma <ondomaambrose19@gmail.com>
2026-01-24 12:56:24 +0100

    Edit 404 page

--- src/pages/404Page.tsx
+++ src/pages/404Page.tsx
@@ -2,16 +2,17 @@ import { Link } from "react-router-dom";
 
 export function PageNotFound() {
   return (
-    <div className="min-h bg-black margin-0 text-white justify-center align-middle">
-      <p className="text-gray-700">Oops!</p>
-      <p className="text-red-700">404</p>
-      <p> Page Not Found </p>
-
-      <Link to="/">
-        <button className="bg-orange-600 text-white text-2xl p-10 border-r-5 hover:bg-orange-700 ">
-          Back Home
-        </button>
-      </Link>
+    <div className="min-h bg-black margin-0 text-white">
+      <div className="p-10 flex flex-col gap-5 justify-center items-center mt-30">
+        <p className="text-gray-700 font-bold">Oops!</p>
+        <p className="text-red-700 text-5xl font-extrabold">404</p>
+        <p> Page Not Found </p>
+        <Link to="/">
+          <button className="bg-orange-600 text-white p-3 border-none hover:bg-orange-700 mt-10">
+            Back Home
+          </button>
+        </Link>
+      </div>
     </div>
   );
 }
